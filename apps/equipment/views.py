9bcc1cba392e1f15b0e5bc75e19a625aaf7fb837
mikhail-luginin <mlugnn@gmail.com>
2023-04-15 20:16:00 +0300

    Add query filter for equipment type page

--- apps/equipment/views.py
+++ apps/equipment/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .models import EquipmentType, Equipment
 from .serializers import EquipmentTypeSerializer, EquipmentSerializer
@@ -7,6 +8,17 @@ class EquipmentTypeViewSet(ModelViewSet):
     queryset = EquipmentType.objects.all()
     serializer_class = EquipmentTypeSerializer
     http_method_names = ['get']
+    
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset, many=True)
+
+        name = request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+            serializer = self.serializer_class(queryset, many=True)
+        
+        return Response(serializer.data)
     
 
 class EquipmentViewSet(ModelViewSet):
