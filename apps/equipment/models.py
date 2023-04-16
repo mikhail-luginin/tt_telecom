657bc5697bb9ce39ea04c3f98980404db999dd7f
mikhail-luginin <mlugnn@gmail.com>
2023-04-16 05:32:15 +0300

    Remove PEP8 warning

--- apps/equipment/models.py
+++ apps/equipment/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+import re
 
 
 class EquipmentType(models.Model):
     name = models.CharField(max_length=128)
     sn_mask = models.CharField(max_length=10)
 
+
 class Equipment(models.Model):
     equipment_type = models.ForeignKey(EquipmentType, on_delete=models.CASCADE)
     serial_number = models.CharField(unique=True, max_length=10)
     note = models.TextField(blank=True, null=True)
     is_deleted = models.BooleanField(default=False)
-    
+
     def soft_delete(self):
         self.is_deleted = True
         self.save()
-
